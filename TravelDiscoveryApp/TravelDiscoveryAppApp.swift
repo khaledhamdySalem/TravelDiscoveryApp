70f7714da2efd50851dae74403d8e3bfdd70acdf
Khaled <Khaled.hamdy2290@gmail.com>
2023-07-20 10:24:45 +0300

    Initial Commit

---
+++ TravelDiscoveryApp/TravelDiscoveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryAppApp.swift
+//  TravelDiscoveryApp
+//
+//  Created by Khaled Hamdy on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
